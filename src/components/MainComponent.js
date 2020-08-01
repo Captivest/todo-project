3c4e3a4391188a1b0b1e8911873b7a65f98df9ee
aniket1020 <aniket.1020@gmail.com>
2020-08-01 12:04:17 +0530

    Initial Setup

---
+++ src/components/MainComponent.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Container} from 'reactstrap';
+
+class Main extends Component{
+	render(){
+		return(
+			<Container fluid>
+			<p>Hello</p>
+			</Container>
+		)
+	}
+}
+
+export default Main;
